fd9f434f152054391eb62c6257fe2694b6dc5772
ErickLopez97 <erick029714@gmail.com>
2016-08-03 05:36:39 -0600

    Add files via upload

---
+++ Ejercicio 5/Ejercicio 5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int v_radio, v_perimetro, v_area;
+            Console.WriteLine("Ingrese el Radio del Circulo");
+            v_radio= int.Parse(Console.ReadLine());
+
+            v_perimetro = 2 * (355 / 113) * v_radio;            
+            Console.WriteLine("El perimetro es:   " + v_perimetro);
+            v_area = (355 / 113) * v_radio*v_radio;
+            Console.WriteLine("El area es:        " + v_area);
+            Console.ReadKey();
+        }
+    }
+}
